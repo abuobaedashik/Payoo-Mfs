ac20c59e6edf09b7f4be8dc532a9c436a3b29b71
Abuobaed <abuobaedashik@gmail.com>
2024-09-22 01:31:16 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-login').addEventListener('click',function (event) {
+    event.preventDefault();
+    // console.log('Login btn clicked')
+    // get the number 
+   const phoneNum =  document.getElementById('phone-number').value;
+   console.log(phoneNum)
+})
