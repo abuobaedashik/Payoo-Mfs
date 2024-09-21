cc67701c55264f990b9c3fcd2858cf26290bbdaa
Abuobaed <abuobaedashik@gmail.com>
2024-09-22 02:25:24 +0600

    second Commit

--- js/login.js
+++ js/login.js
@@ -2,6 +2,14 @@ document.getElementById('btn-login').addEventListener('click',function (event) {
     event.preventDefault();
     // console.log('Login btn clicked')
     // get the number 
-   const phoneNum =  document.getElementById('phone-number').value;
-   console.log(phoneNum)
+   const phoneNum = document.getElementById('phone-number').value;
+   const pinNum = document.getElementById('pin-number').value;
+   
+   if (phoneNum === '0177' && pinNum=== '1234') {
+     console.log('You Are Logged in ');
+     window.location.href='./home.html';
+   }
+   else{
+    alert("wrong Pin or Phone Number")
+   }
 })
